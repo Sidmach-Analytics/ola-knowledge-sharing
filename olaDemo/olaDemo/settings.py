5874b0739b9f0c9aa581a3e70defa2673600b7aa
OlamideTemi <OlamideTemi@users.noreply.github.com>
2021-07-15 14:23:51 +0100

    Changed Debug to False

--- olaDemo/olaDemo/settings.py
+++ olaDemo/olaDemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u_xks(a2%2r^j&z*+wh&88(#*#c7b9b4y#dtdg3*cje9&w^yl%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
